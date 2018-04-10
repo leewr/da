fb4090f9fd953b2c8d75d564b2dbdd71f8edb1f9
liwenrun <liwr@dachentech.com.cn>
2018-04-10 10:29:14 +0800

    picker.vue style

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -7,7 +7,7 @@
         <div class="da-picker__confirm">{{confirmButtonText}}</div>
       </slot>
     </div>
-    <div class="da-picker__column">
+    <div class="da-picker__columns" @touchmove.prevent :style="columnsStyle">
       <PickerColumn
         v-for="(item, index) in currentColumns"
         :key="index"
@@ -18,6 +18,7 @@
       >
       {{item}}
       </PickerColumn>
+      <div class="da-picker__frame da-hairline--top-bottom" :style="frameStyle"></div>
     </div>
   </div>
 </template>
@@ -44,6 +45,10 @@ export default {
     itemHeight: {
       type: Number,
       default: 44
+    },
+    visibleItemCount: {
+      type: Number,
+      default: 5
     }
   },
   data() {
@@ -52,6 +57,16 @@ export default {
     }
   },
   computed: {
+    columnsStyle () {
+      return {
+        height: this.itemHeight * this.visibleItemCount + 'px'
+      }
+    },
+    frameStyle () {
+      return {
+        height: this.itemHeight + 'px'
+      }
+    }
   },
   created () {
     this.initColumns()
@@ -70,12 +85,18 @@ export default {
     },
     valueKey () {
       console.log('1')
+    },
+    columnStyle () {
+      console.log(1)
     }
   }
 }
 </script>
 <style lang="scss">
 .da-picker{
+  overflow: hidden;
+  user-select: none;
+  position: relative;
   .da-picker__toolbar{
     display: flex;
     justify-content: space-between;
@@ -90,5 +111,38 @@ export default {
       padding: 0 15px;
     }
   }
+  .da-picker__columns{
+    position: relative;
+    overflow: hidden;
+  }
+  .da-hairline--top-bottom{
+
+  }
+  .da-picker__frame{
+    top: 50%;
+    left: 0;
+    position: absolute;
+    width: 100%;
+    transform: translateY(-50%);
+    pointer-events:none
+  }
+  .da-hairline--top-bottom::after {
+    border-width: 1px 0;
+  }
+}
+[class*='da-hairline']::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 200%;
+    height: 200%;
+    -webkit-transform: scale(.5);
+    transform: scale(.5);
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    pointer-events: none;
+    box-sizing: border-box;
+    border: 0 solid #e5e5e5;
 }
 </style>
